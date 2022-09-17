374f6273d33d7e06b299f527151665b63c08f65c
Saurav438c <saurav438c@gmail.com>
2022-09-17 09:53:26 +0530

    final submit

--- SRC/controller/internController.js
+++ SRC/controller/internController.js
@@ -1,52 +1,90 @@
 
 const CollegeModel = require("../Model/CollegeModel")
-const internModel = require('../Model/InternModel')
+const InternModel = require('../Model/InternModel')
 
-let checkValid = function (value) {
-    if (typeof value == "undefined" || typeof value == "number" || value.length == 0 || typeof value == null) {
-        return false
-    } else if (typeof value == "string") {
-        return true
-    }
+
+//======================================================================//
+const isValid = function(value){
+    if(typeof (value) === 'undefined' || value === null) return false
+    if(typeof (value) === 'string' && value.trim().length == 0) return false
     return true
 }
 
-const CreateIntern = async function (req, res) {
-    try {
-        let data = req.body;
-        let { name, email, mobile } = data
-        let cName = req.body.collegeName
-
-        if (!(name && email && mobile))
-            return res.status(400).send({ status: false, msg: "Please fill the Mandatory Fields." });
-
-        if (!checkValid(name)) return res.status(400).send({ status: false, message: "Please Provide valid Input" })
-        
-
-
-        if (!checkValid(email)) return res.status(400).send({ status: false, message: "Spaces aren't Allowed." })
-        if (!(/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/).test(email)) { return res.status(400).send({ status: false, msg: "Please provide valid Email" }) }
-        let Duplicate = await internModel.findOne({ email: email })
-        if (Duplicate) { return res.status(409).send({ status: false, msg: "This EmailID already exists please provide another EmailID." }) }
-
-
-        if(!(/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/).test(mobile)) {return res.status(400).send({ status: false, msg: "please enter valid mobile number"})}
-
-
-        let CollageName = await CollegeModel.findOne({name: cName})
-        if(!CollageName) return res.status(400).send({ status: false, msg: "No college found"})
-
-        let collegeId = CollageName._id
-        req.body.collegeId = collegeId
-        delete req.body.CollageName
-        
-        let createIntern = await internModel.create(data)
-        res.status(201).send({ status: true, msg: createIntern })
-
-
-    } catch (error) {
-        res.status(500).send({ error: error.message })
-    }
+const isValidRequestBody = function(reqBody){
+    return Object.keys(reqBody).length > 0
 }
+//======================================================//
+const CreateIntern = async function (req, res){
+
+    try{
+        const requestBody = req.body
+
+
+
+    if(!isValidRequestBody(requestBody)){
+        return res.status(400).send({status: false, message: "please provide input data"})
+    }
+
+    const {name, email, mobile, collegeName} = requestBody
+
+    if(!isValid(name)){
+        return res.status(400).send({status: false, message: "Name must be provided"})
+    }
+
+    if(!isValid(email)){
+        return res.status(400).send({status: false, message: "email must be provided"})
+    }
+    
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+        return res.status(400).send({status: false, message: "enter a valid email"})
+    }
+
+    if(!isValid(mobile)){
+        return res.status(400).send({status: false, message: "mobile must be provided"})
+    }
+
+    if(! /^[6-9]\d{9}$/.test(mobile)){
+        return res.status(400).send({status: false, message: "enter a valid mobile number. like: 7578541254"})
+    }   
+
+    if(!isValid(collegeName)){
+        return res.status(400).send({status: false, message: "collegeName must be provided"})
+    }
+
+    const isEmailNotUnique = await InternModel.findOne({email : email})
+
+    if(isEmailNotUnique){
+        return res.status(400).send({status: false, message: "email already exist"})
+    }
+
+    const isMobileNotUnique = await InternModel.findOne({mobile : mobile})
+
+    if(isMobileNotUnique){
+        return res.status(400).send({status: false, message: "mobile number already exist"})
+    }
+
+    const collegeByCollegeName = await CollegeModel.findOne({name : collegeName})
+
+    if(!collegeByCollegeName){
+        return  res.status(400).send({status: false, message: `no college found by this name: ${collegeName}`})
+    }
+   
+
+    const collegeId = collegeByCollegeName._id
+
+    requestBody.collegeId = collegeId    
+    delete requestBody.collegeName
+
+    const newIntern = await InternModel.create(requestBody)
+
+    res.status(201).send({status: true, message: "new intern entry done", data: newIntern})
+
+
+    }catch (error){
+        res.status(500).send({error : error.message})
+    }
+
+}
+
 
 module.exports.CreateIntern=CreateIntern
